afe8f454733fda7e0467cd78f08139395e85458c
sinar96 <sinarmustikaruslan@gmail.com>
2016-07-29 16:56:22 +0800

    CRUD2

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 import os
 
 # Development (Debug -> True)
+
 DEBUG =True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__name__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql://sinar96:sinar123@localhost/absen'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:sinar123@localhost/absen'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
